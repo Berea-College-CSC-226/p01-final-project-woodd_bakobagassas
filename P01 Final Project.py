0534897d9481c6d440594cd11317b21949a1a228
woodd <woodd@berea.edu>
2025-04-06 21:38:59 -0400

    completed Milestone 1 and did the CRC cards; Created the def draw_X function

---
+++ P01 Final Project.py
@@ -0,0 +1,39 @@
+######################################################################
+# Author: David Wood, Sara Bako
+# Username: woodd,bakobagassas
+#
+# P01 - Final Project
+#
+# Purpose: To Make a tic tac toe game against the computer
+#
+#
+#######################################################################
+####################################################################################
+import turtle
+import tkinter as tk
+
+def draw_x(center_x, center_y, size):
+    """Draws an 'X'  with the given size."""
+    half_size = size / 2
+    turtle.penup()
+
+    # Line from top-left to bottom-right
+    turtle.goto(center_x - half_size, center_y + half_size)
+    turtle.pendown()
+    turtle.goto(center_x + half_size, center_y - half_size)
+
+    # Line from bottom-left to top-right
+    turtle.penup()
+    turtle.goto(center_x - half_size, center_y - half_size)
+    turtle.pendown()
+    turtle.goto(center_x + half_size, center_y + half_size)
+
+    turtle.penup()
+
+
+
+
+
+def main():
+    draw_x(0,0,100)
+main()
