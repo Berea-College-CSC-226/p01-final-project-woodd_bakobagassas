5c9217dec7e871428b0371292646b77dfb0f6b17
bakobagassas <bakobagassas@berea.edu>
2025-04-10 20:31:45 -0400

    The user can now pick a color and symbol

--- OurCode.py
+++ OurCode.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+from tkinter import colorchooser, simpledialog
 #import turtle #commented this out because it creates a whole new screen
 
 class Level1:
@@ -8,21 +9,25 @@ class Level1:
         self.root.title("Tic_Tac_Toe game")
         self.buttons = []
         self.create_buttons()
+        self.choose_color_and_symbol()
+        self.computer_color()
+        self.computer_symbol()
 
     def create_buttons(self):
         for i in range(3):
             row_buttons = []
             for j in range(3):
                 # assigning the buttons, defining the grid's dimensions and calling the human_turn method to ensure the user always goes first
-                OurGrid = tk.Button(self.root, width=20, height=10, command=lambda row=i, column=j: self.human_turn(row, column))
+                OurGrid = tk.Button(self.root, width=20, height=10, bg="lightgrey", command=lambda row=i, column=j: self.human_turn(row, column))
                 OurGrid.grid(row=i, column=j, padx=20, pady=20)
                 row_buttons.append(OurGrid)
             self.buttons.append(row_buttons) #adding all created buttons from the grid to our list of buttons
 
     def human_turn(self, row, column):
         ''' this draws the human initial to the screen'''
-        self.buttons[row][column]['text'] = "X" # writing down the symbol on the button
+        self.buttons[row][column]['text'] = self.human_symbol # writing down the symbol on the button
         self.buttons[row][column]['state'] = 'disabled' # disabling the button
+        self.buttons[row][column]['bg'] = self.human_color
         self.root.after(1000, self.computer_turn)  # searched how to delay before calling
         if self.check_winner():
             return
@@ -30,15 +35,18 @@ class Level1:
     def max_computer_win(self):
         pass
 
-    def UserSymbolAndColor(self):
-        '''search how to change text color, button color and add a user input '''
-        pass
+    def choose_color_and_symbol(self):
+'''this enables the user to choose a color and a symbol'''
+        symbol = simpledialog.askstring("Choose Symbol", "Enter your symbol (e.g. X, G.O.A.T, Tom):")
+        if symbol:
+            self.human_symbol = symbol
 
-    def CompSymbolandColor(self):
-        pass
+        color = colorchooser.askcolor(title="Choose your color")[1]
+        if color:
+            self.human_color = color
 
     def computer_turn(self):
-        '''for now the computer is randomly placing its symbol, until we write a method that maximizes win'''
+
         available_buttons = [(i, j) for i in range(3) for j in range(3) if self.buttons[i][j]['text'] == ''] # checking if which
                                                                         #box is available amongst the total 9 boxes
                                                                         #by checking the boxes that don't have any text in them
@@ -46,6 +54,7 @@ class Level1:
             row, column = random.choice(available_buttons)
             self.buttons[row][column]['text'] = "O" #for now we go with the symbol O. we now need to find a way to make it chose random symbol from a list
             self.buttons[row][column]['state'] = 'disabled' #disabling the use of a button twice
+
             if self.check_winner():
                 return
 
@@ -78,6 +87,7 @@ class Level1:
 
 if __name__ == "__main__":
     root = tk.Tk()
+
     game = Level1(root)
 
     root.mainloop()
